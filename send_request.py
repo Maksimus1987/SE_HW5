ab01ae01c4f7f27ff71430f0c0b0e0fb7c8c3e1c
artemdashkov <artemdashkov@gmail.com>
2024-03-19 19:47:24 +0300

    modified according pep8

--- send_request.py
+++ send_request.py
@@ -1,18 +1,18 @@
-# Разработчик 3: Maksimus1987, отправить POST-запрос на веб-сервер,
-# используя URL и текст, чтобы получить предсказание для данного текста.
-# Затем распечатать ответ в формате JSON
+# Developer 3: Maksimus1987, send a POST request to the web server,
+# using URL and text to get a prediction for a given text.
+# Then print the response in JSON format
 
-import requests  # импортируем модуль `requests`,
-# который предоставляет функции для отправки HTTP-запросов
+import requests  # import the `requests` module,
+# which provides functions for sending HTTP requests
 
-text = "Я очень удивлен происходящим!"  # создаем переменную `text`
-# присваивает ей значение `"Я очень удивлен происходящим!"`
-url = "http://127.0.0.1:8000/predict?text=" + text  # создаем переменную `url
-# и присваивам ей значение `"http://localhost:8080/predict"`
+text = "Я очень удивлен происходящим!"  # create a `text` variable
+# assigns it the value `"I'm very surprised by what's happening!"`
+url = "http://127.0.0.1:8000/predict?text=" + text  # create a variable `url
+# and assign it the value `"http://localhost:8080/predict"`
 
-payload = {"text": text}  # создаем словарь `payload` с ключом `"text"
-# значением `text`. Словарь `payload` будет преобразован в JSON-формат
-# и передан в качестве тела запроса
-response = requests.post(url, json=payload)  # строка отправляет POST-запрос
-# на указанный URL `url` с использованием модуля `requests`
+payload = {"text": text}  # create a dictionary `payload` with the key `"text"
+# with the value `text`. The `payload` dictionary will be converted to JSON format
+# and passed as the request body
+response = requests.post(url, json=payload)  # line sends a POST request
+# to the specified URL `url` using the `requests` module
 print(response.json())
