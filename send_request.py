0e5bf2d16cdab942bb2ca217533e88dd6351a617
Maksimus1987 <149474821+Maksimus1987@users.noreply.github.com>
2024-04-01 16:51:25 +0300

    Update send_request.py
    Внесены изменения для отправки POST-запроса к серверу для получения предсказания по тексту и вывода ответа в формате JSON, а также обработки исключений и проверки статуса кода для надежной обработки ошибок

--- send_request.py
+++ send_request.py
@@ -1,18 +1,26 @@
-# Developer 3: Maksimus1987, send a POST request to the web server,
-# using URL and text to get a prediction for a given text.
-# Then print the response in JSON format
+import requests
 
-import requests  # import the `requests` module,
-# which provides functions for sending HTTP requests
+# Отправка POST-запроса к веб-серверу с предсказанием для заданного текста
+# и вывод ответа в формате JSON
 
-text = "Я очень удивлен происходящим!"  # create a `text` variable
-# assigns it the value `"I'm very surprised by what's happening!"`
-url = "http://127.0.0.1:8000/predict?text=" + text  # create a variable `url
-# and assign it the value `"http://localhost:8080/predict"`
+# Задаем текст для анализа
+text = "Я очень удивлен происходящим!"
 
-payload = {"text": text}  # create a dictionary `payload` with the key `"text"
-# with the value `text`. The `payload` dictionary will be converted to JSON format
-# and passed as the request body
-response = requests.post(url, json=payload)  # line sends a POST request
-# to the specified URL `url` using the `requests` module
-print(response.json())
+# Формируем URL для запроса
+url = f"http://127.0.0.1:8000/predict?text={text}"
+
+# Создаем словарь с текстом для передачи в виде JSON
+payload = {"text": text}
+
+try:
+    # Отправляем POST-запрос
+    response = requests.post(url, json=payload)
+
+    # Проверяем статус код ответа
+    if response.status_code == 200:
+        # Выводим ответ в формате JSON
+        print(response.json())
+    else:
+        print(f"Ошибка при запросе. Код статуса: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Ошибка при отправке запроса: {e}")
