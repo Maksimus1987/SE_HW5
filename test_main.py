e95eadec64c7881f0855a25a68d5a58126fad882
Maksimus1987 <149474821+Maksimus1987@users.noreply.github.com>
2024-04-01 16:53:27 +0300

    Update test_main.py
    Добавлен тестовый случай для /predict точки достъющей, проверяющий код ответа, тип содержимого и правильность предсказаний. Улучшена читаемость кода с помощью добавления комментариев и организации функции тестового случая

--- test_main.py
+++ test_main.py
@@ -1,28 +1,30 @@
-# Developer #1 @aleksrf1 aleksrf@gmail.com - test for api
+# Importing necessary modules
 from fastapi.testclient import TestClient
 from main import app
 
+# Initializing the test client
 client = TestClient(app)
 
-
+# Test case for endpoint /predict
 def test_predict():
-    # Preparing test data
+    # Prepare test data
     text = "Я очень удивлен происходящим!"
 
-    # Execute the request
+    # Make the request
     response = client.post("/predict?text=" + text, json={"text": text})
 
-    # Checking response status
+    # Check response status code
     assert response.status_code == 200
 
-    predictions = response.json()
-
-    # Checking response content type
+    # Validate content type of response
     assert response.headers["content-type"] == "application/json"
 
-    # Checking the correctness of the answer
+    # Check the correctness of the predictions
+    predictions = response.json()
     assert isinstance(predictions, list)
-    if predictions:  # Checking for elements in the predictions list
+
+    if predictions:
         assert isinstance(predictions[0], list)
-    for prob in predictions[0]:
-        assert isinstance(prob, float)
+        
+        for prob in predictions[0]:
+            assert isinstance(prob, float)
